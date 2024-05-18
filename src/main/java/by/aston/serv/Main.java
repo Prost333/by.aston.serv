2bc9b89d29d0c44719a4409b4a432ee635956c73
prost333 <dimabolshoy333@yandex.by>
2024-05-18 11:53:12 +0300

    Initial commit

---
+++ src/main/java/by/aston/serv/Main.java
@@ -0,0 +1,7 @@
+package by.aston.serv;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/by/aston/serv/api/HWServlet.java
@@ -0,0 +1,16 @@
+package by.aston.serv.api;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HWServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello World");
+    }
+}
